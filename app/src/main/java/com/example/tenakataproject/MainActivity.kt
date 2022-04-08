bda0980ded8953b7a01c4adaec73af20340282a4
Junior <onyangogeorge954@gmail.com>
2022-04-08 14:18:49 +0300

    fragment for student details fill in and display section of the list of students

--- app/src/main/java/com/example/tenakataproject/MainActivity.kt
+++ app/src/main/java/com/example/tenakataproject/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.tenakataproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tenakataproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStudent.setOnClickListener{
+
+            replaceFragment(Details())
+
+        }
+
+        binding.btnList.setOnClickListener{
+
+            replaceFragment(Studentlist())
+
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentcontainer,fragment)
+        fragmentTransaction.commit()
+
     }
 }
